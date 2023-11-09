6b2b557630e684bbedcaa2a1a988d50184fdcd03
ibrahimReguigui <ibrahim.reguigui@esprit.tn>
2023-11-09 16:06:29 +0100

    login xml

---
+++ app/src/main/java/com/example/centre_formation/Login.java
@@ -0,0 +1,30 @@
+package com.example.centre_formation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView userName=findViewById(R.id.username);
+        TextView password=findViewById(R.id.password);
+        MaterialButton btnLogin=findViewById(R.id.btnlogin);
+
+        btnLogin.setOnClickListener(e->{
+            if (userName.getText().toString().equals("admin") &&
+            password.getText().toString().equals("admin")){
+                Toast.makeText(this,"Welcome",Toast.LENGTH_SHORT);
+            }else
+                Toast.makeText(this,"Wrong credentials",Toast.LENGTH_LONG);
+        });
+    }
+}
