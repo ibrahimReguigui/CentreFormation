d8ee2d12b803942f81fac89d9b5f8fa75a18404e
HanineBenAyed <hanine.benayed@esprit.tn>
2023-11-12 15:48:51 +0100

    second

--- app/src/main/java/com/example/centre_formation/database/AppDataBase.java
+++ app/src/main/java/com/example/centre_formation/database/AppDataBase.java
@@ -5,11 +5,15 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 import com.example.centre_formation.dao.UserDao;
+import com.example.centre_formation.entity.DateConverter;
+import com.example.centre_formation.entity.Formation;
 import com.example.centre_formation.entity.User;
 
-@Database(entities = {User.class}, version = 1, exportSchema = false)
+@Database(entities = {User.class, Formation.class}, version = 2, exportSchema = false)
+@TypeConverters({DateConverter.class})
 public abstract class AppDataBase extends RoomDatabase {
     private static AppDataBase instance;
     public abstract UserDao userDao();
