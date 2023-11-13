84c183375a2d2463db4839b1e651a760bae0fc71
ibrahimReguigui <ibrahim.reguigui@esprit.tn>
2023-11-13 17:44:57 +0100

    profile and update password

--- app/src/main/java/com/example/centre_formation/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/centre_formation/fragment/ProfileFragment.java
@@ -7,15 +7,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.centre_formation.MainActivity;
 import com.example.centre_formation.R;
 import com.example.centre_formation.database.AppDataBase;
+import com.example.centre_formation.entity.User;
+import com.google.gson.Gson;
 
 
 public class ProfileFragment extends Fragment {
     SharedPreferences myPref;
     AppDataBase database;
+    TextView name,role,email,first,last,adresse,phoneNumber;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +35,25 @@ public class ProfileFragment extends Fragment {
 
         database = AppDataBase.getAppDatabase(getActivity());
 
+        String userJson = myPref.getString("connectedUser", "null");
+        Gson gson = new Gson();
+        User user = gson.fromJson(userJson, User.class);
 
+        first=view.findViewById(R.id.firstNameInProfile2);
+        last=view.findViewById(R.id.lastNameInProfile);
+        email=view.findViewById(R.id.emailInProfile);
+        adresse=view.findViewById(R.id.adresseInProfile);
+        name=view.findViewById(R.id.nameInProfile);
+        role=view.findViewById(R.id.roleInProfile);
+        phoneNumber=view.findViewById(R.id.phoneInProfile);
+
+        first.setText(user.getFirstName());
+        last.setText(user.getLastName());
+        email.setText(user.getEmail());
+        adresse.setText(user.getAdress());
+        name.setText(user.getFirstName()+" "+user.getLastName());
+        role.setText(user.getRole());
+        phoneNumber.setText(String.valueOf(user.getPhoneNumber()));
 
         return view;
     }
